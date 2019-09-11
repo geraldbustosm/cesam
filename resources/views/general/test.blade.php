f89481844efa9585911400bb4aed9e0272a8c132
Matias Bruce <matiasibgr@gmail.com>
2019-09-11 11:15:00 -0300

    tabla bonita

--- resources/views/general/test.blade.php
+++ resources/views/general/test.blade.php
@@ -26,11 +26,71 @@
             <button id="download-xlsx">Descargar XLSX</button>
             <button id="download-pdf">Descargar PDF</button>
         </div>
-        <div id="example-table">
+        <div class="table-controls-legend">Parametros para filtrar</div>
+
+        <div class="table-controls">
+            <span>
+            <label>Field: </label>
+            <select id="filter-field">
+                <option></option>
+                <option value="nombre1">Nombre</option>
+                <option value="apellido1">Apellido</option>
+                
+            </select>
+            </span>
+
+            <span>
+            <label>Tipo: </label>
+            <select id="filter-type">
+                <option value="=">=</option>
+                <option value="<">&lt;</option>
+                <option value="<=">&lt;=</option>
+                <option value=">">&gt;</option>
+                <option value=">=">&gt;=</option>
+                <option value="distinto">!=</option>
+                <option value="igual">like</option>
+            </select>
+            </span>
+
+            <span><label>Valor: </label> <input id="filter-value" type="text" placeholder="valor a filtrar"></span>
+
+            <button id="filter-clear">Limpiar Filtro</button>
+        </div>
+        <div id="example-table"></div>
             <script type="text/javascript">
+            //Trigger setFilter function with correct parameters
+            function updateFilter(){
+
+            var filter = $("#filter-field").val() == "function" ? customFilter : $("#filter-field").val();
+
+            if($("#filter-field").val() == "function" ){
+                $("#filter-type").prop("disabled", true);
+                $("#filter-value").prop("disabled", true);
+            }else{
+                $("#filter-type").prop("disabled", false);
+                $("#filter-value").prop("disabled", false);
+            }
+
+            table.setFilter(filter, $("#filter-type").val(), $("#filter-value").val());
+            }
+            //Update filters on value change
+            $("#filter-field, #filter-type").change(updateFilter);
+            $("#filter-value").keyup(updateFilter);
+
+            //Clear filters on "Clear Filters" button click
+            $("#filter-clear").click(function(){
+                $("#filter-field").val("");
+                $("#filter-type").val("=");
+                $("#filter-value").val("");
+
+                table.clearFilter();
+            });
+
+
             //create Tabulator on DOM element with id "example-table"
             var table = new Tabulator("#example-table", {
                 height:"311px",
+                movableColumns: true,
                 columns:[
                 {title:"Nombre", field:"nombre1"},
                 {title:"Apellido", field:"apellido1"},
@@ -66,7 +126,7 @@
                 });
             });
             </script>
-        </div>
+        
     </form>
 
 @endsection
