694c65c248cf4770258c4bd9bcb576cec0c8f8b3
Gerald Bustos Morales <geraldbustosm@gmail.com>
2019-11-09 00:00:27 -0300

    check role working

--- app/Http/Middleware/CheckRole.php
+++ app/Http/Middleware/CheckRole.php
@@ -16,7 +16,7 @@ class CheckRole
      */
     public function handle($request, Closure $next, $role)
     {
-        if(Auth::user()->rol != $role){
+        if(Auth::user()->rol > $role){
             return redirect('/');
         }
         return $next($request);
