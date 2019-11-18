161e04021d2a347e9459f76ccdb2555b01d90869
Rodrigo Dorat <rdoratm@outlook.com>
2019-11-18 15:51:36 -0300

    Fixing button showup only on active clinicalRecord and redirect to addAttendance on addRelease function of controller

--- app/Http/Controllers/GeneralController.php
+++ app/Http/Controllers/GeneralController.php
@@ -68,8 +68,8 @@ class GeneralController extends Controller
         $patient_id = $patient->id;
         // Get the stage
         $stage = Stage::where('paciente_id', $patient_id)
-                    ->where('activa', 1)
-                    ->first();
+            ->where('activa', 1)
+            ->first();
         // If have no active stage
         if (empty($stage)) {
             return redirect(url()->previous())->with('error', 'Debe agregar una nueva etapa');
@@ -85,33 +85,37 @@ class GeneralController extends Controller
     /***************************************************************************************************************************
                                                     OTHER PROCESS
      ****************************************************************************************************************************/
-    public function stagesPerPatient(Request $request){
+    public function stagesPerPatient(Request $request)
+    {
         $id = $request->id;
         $stages = Stage::where('paciente_id', $id)->where('activa', 0)->get();
         // $stages = Stage::all();
-        return response()->json($stages);  
+        return response()->json($stages);
     }
 
-    public function selectStage(Request $request){
+    public function selectStage(Request $request)
+    {
         $patient_id = $request->id;
         $patient = Patient::find($patient_id);
         $stage_id = $request->stages;
         $stage = Stage::find($stage_id);
         $patientAtendances = $stage->attendance;
         $activeStage = Stage::where('paciente_id', $patient_id)
-                            ->where('activa', 1)
-                            ->select('id')
-                            ->first();
+            ->where('activa', 1)
+            ->select('id')
+            ->first();
         return view('General.clinicalRecords', compact('patient', 'stage', 'patientAtendances', 'activeStage'));
     }
 
-    public function showAddRelease($DNI){
+    public function showAddRelease($DNI)
+    {
         $DNI = $DNI;
         $release = Release::where('activa', 1)->orderBy('descripcion')->get();
         return view('general.clinicalRelease', compact('DNI', 'release'));
     }
 
-    public function addRelease(Request $request){
+    public function addRelease(Request $request)
+    {
         // Check the format of each variable of 'request'
         $validacion = $request->validate([
             'DNI' => 'required|string|max:255',
@@ -121,12 +125,16 @@ class GeneralController extends Controller
         $patient = Patient::where('DNI', $DNI)->first();
         $patient_id = $patient->id;
         $stage = Stage::where('paciente_id', $patient_id)
-                    ->where('activa', 1)
-                    ->first();
-        $stage->activa = 0;
-        $stage->alta_id = $request->releases;
-        $stage->save();
-        $url = "alta/".$DNI;
-        return redirect($url)->with('status', 'Paciente '.$DNI.' fue dado de alta');
+            ->where('activa', 1)
+            ->first();
+        if (empty($stage)) {
+            return app('App\Http\Controllers\StageController')->showAddStage($patient_id);
+        } else {
+            $stage->activa = 0;
+            $stage->alta_id = $request->releases;
+            $stage->save();
+            $url = "alta/".$DNI;
+            return redirect($url)->with('status', 'Paciente ' . $DNI . ' fue dado de alta');
+        }
     }
 }
--- resources/views/general/clinicalRecords.blade.php
+++ resources/views/general/clinicalRecords.blade.php
@@ -24,37 +24,42 @@
     <p><b>Paciente: </b>{{ $patient->nombre1 }} {{ $patient->apellido1 }} {{ $patient->apellido2 }}</p>
     <p class="mb-2"><b>Rut: </b>{{ $patient->DNI}}</p>
     <button type="button" class="btn btn-primary mb-2" id="addPrevition">Añadir prestación</button>
+    @if($stage->activa == 1)
     <button type="button" class="btn btn-primary mb-2" id="addRelease">Dar alta</button>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+    @endif
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
         Soluta excepturi hic odit saepe. Distinctio exercitationem
         totam tempore iste quae, ea quo voluptates unde quos, quod
         velit assumenda quia corrupti quidem consectetur doloribus
         saepe optio impedit rem dolor nisi quisquam a beatae quasi!
         Sed dignissimos harum iste delectus ducimus eum sint.
-        Mollitia labore harum libero, blanditiis dolores ipsa ipsam quia sapiente?<p>
+        Mollitia labore harum libero, blanditiis dolores ipsa ipsam quia sapiente?
+    <p>
 
-            @php
-            $stageCount = $stage->attendance->count();
-            @endphp
-            @foreach($patientAtendances as $value)
-            <div class="card">
-                <div class="card-header">
-                    <div>Prestación #{{$stageCount}} </div>
-                    <div>
-                        <a class="" href="{{url('#')}}"><i class="material-icons">create</i><span></span></a>
-                        <a class="" href="{{url('#')}}"><i class="material-icons">delete</i><span></span></a>
-                    </div>
-                </div>
-                <div class="card-body">
-                    <h6 class="card-subtitle mb-2 text-muted">Profesional: {{ $value->functionary->user->primer_nombre }}, {{ $value->functionary->profesion}}</h6>
-                    <h6 class="card-subtitle mb-4 text-muted">{{ $value->fecha}}</h6>
-                    <h6 class="card-subtitle text-muted">{{ $value->provision->glosaTrasadora}}</p>
+    @php
+    $stageCount = $stage->attendance->count();
+    @endphp
+    @foreach($patientAtendances as $value)
+    <div class="card">
+        <div class="card-header">
+            <div>Prestación #{{$stageCount}} </div>
+            <div>
+                <a class="" href="{{url('#')}}"><i class="material-icons">create</i><span></span></a>
+                <a class="" href="{{url('#')}}"><i class="material-icons">delete</i><span></span></a>
+            </div>
+        </div>
+        <div class="card-body">
+            <h6 class="card-subtitle mb-2 text-muted">Profesional: {{ $value->functionary->user->primer_nombre }}, {{ $value->functionary->profesion}}</h6>
+            <h6 class="card-subtitle mb-4 text-muted">{{ $value->fecha}}</h6>
+            <h6 class="card-subtitle text-muted">{{ $value->provision->glosaTrasadora}}</p>
+        </div>
+    </div>
+    @php
+        $stageCount = $stageCount - 1;
+    @endphp
+    @endforeach
 </div>
-</div>
-@php
-$stageCount = $stageCount - 1;
-@endphp
-@endforeach
 <script>
     $(document).ready(function() {
         // Run code
