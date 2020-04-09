ebb0ba4895b8840f7f3064da3d68b8d6ffdb5fb8
matiasBGR <matiasibgr@gmail.com>
2020-04-08 20:31:46 -0400

    cambio de nombre redundante en CanastaController

--- app/Http/Controllers/CanastaController.php
+++ app/Http/Controllers/CanastaController.php
@@ -28,7 +28,7 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 
-class AdminController extends Controller
+class CanastaController extends Controller
 {
     /**
      * Create a new controller instance.
